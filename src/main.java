81905968cb51596f2dcfe447fd4105e4af689822
dairoquintero <48736685+dairoquintero@users.noreply.github.com>
2020-09-06 11:54:47 -0500

    test lumu technologies

--- src/main.java
+++ src/main.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class main {
 
@@ -51,22 +50,41 @@ public class main {
         List<Integer> counts = new ArrayList<Integer>();
         //text without character \n
         String notBreakRow = textPreprocessed.replaceAll("\n", "");
+
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
             int counter = 0;
-            if (!flags.contains(word)) {
+            if (!flags.contains(word.toLowerCase())) {
                 for (int j = i; j < words.length; j++) {
-                    if (word.equals(words[j])) {
+                    if (word.equalsIgnoreCase(words[j])) {
                         counter++;
                     }
                 }
-                flags.add(word);
+                flags.add(word.toLowerCase());
                 counts.add(counter);
             }
+
         }
 
+        //order descendent
+        int i, j, aux;
+        String auxString;
+
+
+        for (i = 0; i < counts.size() - 1; i++)
+            for (j = 0; j < counts.size() - i - 1; j++)
+                if (counts.get(j + 1) > counts.get(j)) {
+                    aux = counts.get(j + 1);
+                    auxString = flags.get(j + 1);
+                    counts.set(j + 1, counts.get(j));
+                    flags.set(j + 1, flags.get(j));
+                    counts.set(j, aux);
+                    flags.set(j, auxString);
+                }
+
+
         String response = words.length + " words\n" + notBreakRow.length() + " characters\n\n";
-        for (int i = 0; i < flags.size(); i++) {
+        for (i = 0; i < flags.size(); i++) {
             response += flags.get(i) + ": " + counts.get(i) + "\n";
         }
         return response;
